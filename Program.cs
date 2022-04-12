5038c8cb7752df9b474d7497a1968da4b8833999
Giorgio Vilardo <giorgio.vilardo@maieuticallabs.it>
2022-04-12 21:25:52 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string ReturnPersonName(Person p) => p.Name;
+string ReturnPersonTwoName(PersonTwo p) => p.Name;
+
+var prsn = new Person();
+var prsn2 = new PersonTwo();
+
+// Doesn't warn, but crashes at runtime
+// type is string tho, not string?
+prsn.Name = null;
+// Warn
+prsn2.Name = null;
+
+Console.WriteLine($"*|{ReturnPersonName(prsn)}|*");
+Console.WriteLine($"*|{ReturnPersonTwoName(prsn2)}|*");
+
+class PersonTwo
+{
+    public string Name { get; set; } = "";
+}
